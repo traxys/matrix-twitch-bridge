d7dc09a2c553ecfbe85313b7faff008588d538e4
traxys <quentin+dev@familleboyer.net>
2024-07-07 15:55:55 +0200

    Basic appservice

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,102 @@
-fn main() {}
+use std::{path::PathBuf, sync::Arc};
+
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Router};
+use ruma::api::{
+    appservice::ping::send_ping,
+    client::{
+        appservice::request_ping,
+        error::{ErrorBody, ErrorKind},
+    },
+};
+use serde::{Deserialize, Serialize};
+use tracing_subscriber::EnvFilter;
+
+type HttpClient = ruma::client::http_client::Reqwest;
+
+#[allow(dead_code)]
+mod utils;
+
+use utils::{RumaRequest, RumaResponse};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+struct Config {
+    homeserver_url: String,
+    registration_file: PathBuf,
+}
+
+impl Config {
+    fn new() -> anyhow::Result<Self> {
+        Ok(envious::Config::default()
+            .with_prefix("MATRIX_TWITCH_")
+            .case_sensitive(true)
+            .build_from_env()?)
+    }
+}
+
+async fn ping(
+    state: State<Arc<BridgeState>>,
+    request: RumaRequest<send_ping::v1::Request>,
+) -> impl IntoResponse {
+    tracing::info!("Got ping: {request:?}");
+
+    if request.hs_token != state.hs_token {
+        return RumaResponse(ruma::api::client::Error::new(
+            StatusCode::FORBIDDEN,
+            ErrorBody::Standard {
+                kind: ErrorKind::forbidden(),
+                message: "hs_token mismatch".into(),
+            },
+        ))
+        .into_response();
+    } else {
+        RumaResponse(send_ping::v1::Response::new()).into_response()
+    }
+}
+
+struct BridgeState {
+    _client: ruma::Client<HttpClient>,
+    hs_token: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_target(true)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let config = Config::new()?;
+
+    let registration = std::fs::read_to_string(config.registration_file)?;
+    let registration: ruma::api::appservice::Registration = serde_yml::from_str(&registration)?;
+
+    let client = ruma::Client::builder()
+        .homeserver_url(config.homeserver_url)
+        .access_token(Some(registration.as_token))
+        .build::<HttpClient>()
+        .await?;
+
+    let state = Arc::new(BridgeState {
+        _client: client.clone(),
+        hs_token: registration.hs_token,
+    });
+
+    let app = Router::new()
+        .route("/_matrix/app/v1/ping", post(ping))
+        .with_state(state);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8009").await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = client
+            .send_request(request_ping::v1::Request::new("twitch-bridge".into()))
+            .await
+        {
+            tracing::error!("ping failed: {e:#?}");
+            std::process::exit(1);
+        }
+    });
+
+    axum::serve(listener, app).await.map_err(|e| e.into())
+}
---
+++ src/utils.rs
@@ -0,0 +1,97 @@
+use axum::{
+    async_trait,
+    extract::{FromRequest, RawPathParams, Request},
+    http::StatusCode,
+    response::IntoResponse,
+    RequestExt,
+};
+use axum_extra::{
+    headers::{authorization::Bearer, Authorization},
+    TypedHeader,
+};
+use bytes::{Bytes, BytesMut};
+use http_body_util::BodyExt;
+use ruma::api::{
+    client::{self, error::ErrorBody},
+    IncomingRequest, OutgoingResponse,
+};
+
+#[derive(Debug)]
+pub struct RumaRequest<T> {
+    pub request: T,
+    pub hs_token: String,
+}
+
+#[async_trait]
+impl<T, S> FromRequest<S> for RumaRequest<T>
+where
+    T: IncomingRequest,
+    S: Send + Sync,
+{
+    type Rejection = axum::response::Response;
+
+    async fn from_request(mut req: Request, _: &S) -> Result<Self, Self::Rejection> {
+        let hs_token = match req
+            .extract_parts::<TypedHeader<Authorization<Bearer>>>()
+            .await
+        {
+            Ok(v) => v.0 .0.token().to_owned(),
+            Err(e) => {
+                tracing::warn!("Malformed hs token: {e:?}");
+                return Err(RumaResponse(client::Error::new(
+                    StatusCode::FORBIDDEN,
+                    ErrorBody::Standard {
+                        kind: client::error::ErrorKind::forbidden(),
+                        message: "malformed hs token".into(),
+                    },
+                ))
+                .into_response());
+            }
+        };
+
+        // TODO: use the correct matrix error response
+        let path_args = req
+            .extract_parts::<RawPathParams>()
+            .await
+            .map_err(|e| e.into_response())?
+            .into_iter()
+            .map(|(_, value)| value.to_owned())
+            .collect::<Vec<_>>();
+
+        // TODO: use the correct matrix error response
+        let (head, body) = req.into_parts();
+        let body = body
+            .collect()
+            .await
+            .map_err(|_| StatusCode::BAD_REQUEST.into_response())?
+            .to_bytes();
+
+        match T::try_from_http_request(Request::from_parts(head, body), &path_args) {
+            Ok(request) => Ok(RumaRequest { request, hs_token }),
+            Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string()).into_response()),
+        }
+    }
+}
+
+pub struct RumaResponse<T>(pub T);
+
+impl<T> IntoResponse for RumaResponse<T>
+where
+    T: OutgoingResponse,
+{
+    fn into_response(self) -> axum::response::Response {
+        match OutgoingResponse::try_into_http_response::<BytesMut>(self.0) {
+            Ok(rsp) => rsp.map(|b| Bytes::from(b).into()),
+            Err(e) => {
+                tracing::error!("Error while creating response: {e:?}");
+                StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            }
+        }
+    }
+}
+
+impl<T: OutgoingResponse> From<T> for RumaResponse<T> {
+    fn from(v: T) -> Self {
+        Self(v)
+    }
+}
